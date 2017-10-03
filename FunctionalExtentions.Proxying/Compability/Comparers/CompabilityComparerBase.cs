228e5ecae7c228247c156cb54194c3162624e219
ivansivoplias <ivan.kozaks@gmail.com>
2017-10-03 18:29:18 +0300

    Refactor project structure.

---
+++ FunctionalExtentions.Proxying/Compability/Comparers/CompabilityComparerBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace FunctionalExtentions.Proxying.Compability
+{
+    public abstract class CompabilityComparerBase : ICompabilityComparer
+    {
+        protected const BindingFlags All =
+            BindingFlags.Public |
+            BindingFlags.NonPublic |
+            BindingFlags.Static |
+            BindingFlags.Instance;
+
+        public abstract MemberTypes TargetMemberType { get; }
+
+        public abstract bool IsCompatible(object one, object other);
+    }
+}
