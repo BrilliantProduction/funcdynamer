ebae9f14fc0d78d84f32522b02254e777c71388a
ivansivoplias <ivan.kozaks@gmail.com>
2017-11-08 08:24:35 +0200

    Start implementing sorting helpers. Added CollectionExtentions class.

---
+++ FunctionalExtentions.ValueCollections/Helpers/CollectionExtentions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalExtentions.ValueCollections.Helpers
+{
+    public enum CollectionHalf
+    {
+        First,
+        Second
+    }
+
+    public static class CollectionExtentions
+    {
+        public static void CopyTo<T>(this ICollection<T> source, ICollection<T> destination)
+        {
+            destination.Clear();
+            foreach (var item in source)
+            {
+                destination.Add(item);
+            }
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+
+        public static ICollection<T> GetHalf<T>(this ICollection<T> collection, CollectionHalf half)
+        {
+            var list = new List<T>();
+            var sourceArray = collection.ToArray();
+
+            int length = half == CollectionHalf.First ? (collection.Count >> 1) + 1 : collection.Count;
+            int startIndex = half == CollectionHalf.First ? 0 : (collection.Count >> 1) + 1;
+
+            for (int i = startIndex; i < length; i++)
+            {
+                list.Add(sourceArray[i]);
+            }
+            return list;
+        }
+    }
+}
---
+++ FunctionalExtentions.ValueCollections/Sorting/SortingAlgorithm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalExtentions.ValueCollections.Sorting
+{
+    public abstract class SortingAlgorithm
+    {
+        #region Collections sorting
+
+        public abstract void Sort<T>(ICollection<T> source, IComparer<T> comparer = null);
+
+        public abstract void Sort<T>(ICollection<T> source, Comparison<T> comparison);
+
+        public abstract void Sort<T>(ICollection<T> source)
+            where T : IComparable<T>;
+
+        #endregion Collections sorting
+
+        #region Any enumerable sorting
+
+        public abstract IEnumerable<T> Sort<T>(IEnumerable<T> source, IComparer<T> comparer = null);
+
+        public abstract IEnumerable<T> Sort<T>(IEnumerable<T> source, Comparison<T> comparison);
+
+        public abstract IEnumerable<T> Sort<T>(IEnumerable<T> source)
+            where T : IComparable<T>;
+
+        #endregion Any enumerable sorting
+    }
+}
