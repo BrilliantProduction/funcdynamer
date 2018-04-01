d744614b7a9b20a519395644a60f56ba47b61e71
ivansivoplias <ivan.kozaks@gmail.com>
2018-04-01 09:28:11 +0300

    Moved RedBlackTreeNode to separate class.

--- FunctionalExtentions.ValueCollections/Trees/RedBlackTree.cs
+++ FunctionalExtentions.ValueCollections/Trees/RedBlackTree.cs
@@ -15,44 +15,12 @@ namespace FunctionalExtentions.ValueCollections.Trees
         where TKey : IComparable<TKey>
     {
 
-        private RedBlackTreeNode _root;
+        private RedBlackTreeNode<TKey, TValue> _root;
 
-        /// <summary>
-        /// Node of the red-black tree
-        /// </summary>
-        protected class RedBlackTreeNode
+        public void Put(TKey key, TValue value)
         {
-            private TKey _key;
-            private TValue _value;
-            private bool _isRed;
-
-            private RedBlackTreeNode _left, _right;
-
-            public RedBlackTreeNode(TKey key, TValue value, bool isRed)
-            {
-                _key = key;
-                _value = value;
-                _isRed = isRed;
-            }
-
-            public TKey Key { get => _key; set => _key = value; }
-
-            public TValue Value { get => _value; set => _value = value; }
-
-            public bool IsRed { get => _isRed; set => _isRed = value; }
-
-            protected RedBlackTreeNode Left
-            {
-                get => _left;
-                set => _left = value;
-            }
-
-            protected RedBlackTreeNode Right
-            {
-                get => _right;
-                set => _right = value;
-            }
-
+            if (_root == null)
+                _root = new RedBlackTreeNode<TKey, TValue>(key, value, false);
         }
     }
 }
---
+++ FunctionalExtentions.ValueCollections/Trees/RedBlackTreeNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalExtentions.ValueCollections.Trees
+{
+    /// <summary>
+    /// Node of the red-black tree
+    /// </summary>
+    public class RedBlackTreeNode<TKey, TValue>
+    {
+        private TKey _key;
+        private TValue _value;
+        private bool _isRed;
+
+        private RedBlackTreeNode<TKey, TValue> _left, _right;
+
+        public RedBlackTreeNode(TKey key, TValue value, bool isRed)
+        {
+            _key = key;
+            _value = value;
+            _isRed = isRed;
+        }
+
+        public TKey Key { get => _key; set => _key = value; }
+
+        public TValue Value { get => _value; set => _value = value; }
+
+        public bool IsRed { get => _isRed; set => _isRed = value; }
+
+        public bool IsLeaf => Left == null && Right == null;
+
+        public RedBlackTreeNode<TKey, TValue> Left
+        {
+            get => _left;
+            set => _left = value;
+        }
+
+        public RedBlackTreeNode<TKey, TValue> Right
+        {
+            get => _right;
+            set => _right = value;
+        }
+
+    }
+}
