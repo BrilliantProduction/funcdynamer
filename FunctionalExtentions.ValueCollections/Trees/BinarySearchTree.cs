3247db753d66356bd8a6fe7685e2f6a97edd1308
ivansivoplias <ivan.kozaks@gmail.com>
2018-04-04 07:02:33 +0300

    Start binary search tree implementation.

---
+++ FunctionalExtentions.ValueCollections/Trees/BinarySearchTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalExtentions.ValueCollections.Trees
+{
+    /// <summary>
+    /// Binary search tree implementation
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
+    public class BinarySearchTree<TKey, TValue>
+    {
+        private class Node<TKey, TValue>
+        {
+            private TKey _key;
+            private TValue _value;
+
+            private Node<TKey, TValue> _left, right;
+        }
+    }
+}
