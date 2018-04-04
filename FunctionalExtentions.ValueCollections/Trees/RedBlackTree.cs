d3159f65874045627e8edfbe3d5508e468625182
ivansivoplias <ivan.kozaks@gmail.com>
2018-04-04 06:55:00 +0300

    Added method stubs to RebBlackTree.

--- FunctionalExtentions.ValueCollections/Trees/RedBlackTree.cs
+++ FunctionalExtentions.ValueCollections/Trees/RedBlackTree.cs
@@ -14,7 +14,6 @@ namespace FunctionalExtentions.ValueCollections.Trees
     public class RedBlackTree<TKey, TValue>
         where TKey : IComparable<TKey>
     {
-
         private RedBlackTreeNode<TKey, TValue> _root;
 
         public void Put(TKey key, TValue value)
@@ -22,5 +21,28 @@ namespace FunctionalExtentions.ValueCollections.Trees
             if (_root == null)
                 _root = new RedBlackTreeNode<TKey, TValue>(key, value, false);
         }
+
+        public TValue Get(TKey key)
+        {
+            return default(TValue);
+        }
+
+        public void Remove(TKey key)
+        {
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return false;
+        }
+
+        public bool IsEmpty()
+        {
+            return false;
+        }
+
+        public int Count => 0;
+
+        public IEnumerable<TKey> Keys { get; }
     }
 }
